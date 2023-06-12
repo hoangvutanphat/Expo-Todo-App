f885612e38fe0f40b8ecf5945c91816a0be2e775
Hoang Vu Tan Phat <hoangvutanphat.tn92@gmail.com>
2023-06-12 08:27:46 +0700

    (fix)Delete coderedux in file app

--- App.tsx
+++ App.tsx
@@ -2,16 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import AddForm from './src/component/AddForm';
 import { Provider } from 'react-redux';
-import { store } from './src/app/store'
+
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <View style={styles.container}>
-        <AddForm></AddForm>
-        <StatusBar style="auto" />
-      </View>
-    </Provider>
+
+    <View style={styles.container}>
+      <AddForm></AddForm>
+      <StatusBar style="auto" />
+    </View>
+
   );
 }
 const styles = StyleSheet.create({
